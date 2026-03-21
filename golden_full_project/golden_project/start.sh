de41680f27e4a66a5360ce273f06b6a0f2499ab7
Mourchid KARIMOU <mourchidkarimou4@gmail.com>
2026-03-21 04:53:33 +0100

    fix: remove createsuperuser from start.sh, fix migrate order

--- golden_full_project/golden_project/start.sh
+++ golden_full_project/golden_project/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-python manage.py migrate --noinput
+python manage.py migrate --run-syncdb --noinput
 python manage.py collectstatic --noinput
-python manage.py createsuperuser --noinput || true
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
