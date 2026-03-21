2cc5d09e0b9a2731a4a55409dabf07bbfae35ddb
Mourchid KARIMOU <mourchidkarimou4@gmail.com>
2026-03-21 05:04:27 +0100

    fix: migrate in correct order contenttypes > auth > all

--- golden_full_project/golden_project/start.sh
+++ golden_full_project/golden_project/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 cd /opt/render/project/src/golden_full_project/golden_project
-python manage.py migrate --run-syncdb --noinput
+python manage.py migrate contenttypes --noinput
+python manage.py migrate auth --noinput
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
