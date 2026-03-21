9823c929e7b99565920cf7af4371dd4b91c4b05a
Mourchid KARIMOU <mourchidkarimou4@gmail.com>
2026-03-21 04:00:08 +0100

    fix: add collectstatic and migrate in start script

--- golden_full_project/golden_project/start.sh
+++ golden_full_project/golden_project/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput || true
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
