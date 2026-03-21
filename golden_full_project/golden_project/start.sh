32659fb42ec6a1e1c6315e01a1483653f233d145
Mourchid KARIMOU <mourchidkarimou4@gmail.com>
2026-03-21 04:58:27 +0100

    fix: force redeploy with correct start script

--- golden_full_project/golden_project/start.sh
+++ golden_full_project/golden_project/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+cd /opt/render/project/src/golden_full_project/golden_project
 python manage.py migrate --run-syncdb --noinput
 python manage.py collectstatic --noinput
-gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 2 --timeout 120
