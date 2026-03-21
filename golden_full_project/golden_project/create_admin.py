4b949dffc9b7595b0351ea2b9424d05f92b90823
Mourchid KARIMOU <mourchidkarimou4@gmail.com>
2026-03-21 06:51:31 +0100

    feat: add create_admin script

---
+++ golden_full_project/golden_project/create_admin.py
@@ -0,0 +1,14 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
+django.setup()
+from apps.users.models import User
+if not User.objects.filter(email='admin@golden.com').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@golden.com',
+        password='Golden2026!'
+    )
+    print('Superuser créé !')
+else:
+    print('Superuser existe déjà.')
